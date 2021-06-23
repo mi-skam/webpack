168c7b0526e950061f005b04effe041e84268618
mi-skam <maksim.codes@mailbox.org>
2021-06-23 18:15:04 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let pkgs = nixpkgs.legacyPackages.${system};
+       nixConfig.bash-prompt = "\[devShell\]$ ";
+  
+      in {
+        devShell = pkgs.mkShell {
+          name = "smart3network";
+          buildInputs = [ pkgs.nodejs-14_x ]; 
+          shellHook = ''
+            export PATH="/home/plumps/.npm-packages/bin:$PATH"
+          '';
+
+          NPM_CONFIG_PREFIX = "/home/plumps/.npm-packages";
+        };
+      });
+}
